00fc916aee8e0ef9f3035bf71a2078380147eb52
azady <azady@SYSTEMOFADOWN>
2025-02-09 21:02:50 +0300

    <son commit>

--- MyWebSite/Controllers/CategoryController.cs
+++ MyWebSite/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,20 +14,43 @@ namespace MyWebSite.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
-        CategoryManager cm = new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult GetCategoryList()
         {
-            var categoryvalues = cm.GetAll();
+            var categoryvalues = cm.GetList();
             return View(categoryvalues);
         }
+        [HttpGet]
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
         public ActionResult AddCategory(Category p)
         {
-            cm.CategoryAddBL(p);
-            return RedirectToAction("GetCategoryList");
+            //cm.CategoryAddBL(p);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
